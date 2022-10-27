0bc80e8a1c61ba7a265517ff0b8fe0c4af299f81
YASOOCODE <yaseensaadeldinmohamed@gmail.com>
2022-10-27 15:23:39 +0200

    Done

---
+++ main.js
@@ -0,0 +1,79 @@
+let cells = document.querySelectorAll("body main section article"),
+  turn = "X",
+  turnP = document.querySelector("p");
+
+for (let i = 0; i < cells.length; i++) {
+  let cel = cells[i];
+  cel.onclick = (e) => {
+    if (e.target.innerHTML === "" && turn == "X") {
+      e.target.innerHTML = "X";
+      turn = "O";
+      turnP.innerText = "Its O Turn";
+    } else if (e.target.innerHTML === "" && turn == "O") {
+      e.target.innerHTML = "O";
+      turn = "X";
+      turnP.innerText = "Its X Turn";
+    } else {
+      cel.classList.add("shake");
+      setTimeout(() => {
+        cel.classList.remove("shake");
+      }, 1000);
+    }
+    if (
+      (cells[0].innerHTML == "X" &&
+        cells[1].innerHTML == "X" &&
+        cells[2].innerHTML == "X") ||
+      (cells[0].innerHTML == "X" &&
+        cells[3].innerHTML == "X" &&
+        cells[6].innerHTML == "X") ||
+      (cells[1].innerHTML == "X" &&
+        cells[4].innerHTML == "X" &&
+        cells[7].innerHTML == "X") ||
+      (cells[2].innerHTML == "X" &&
+        cells[5].innerHTML == "X" &&
+        cells[8].innerHTML == "X") ||
+      (cells[3].innerHTML == "X" &&
+        cells[4].innerHTML == "X" &&
+        cells[5].innerHTML == "X") ||
+      (cells[6].innerHTML == "X" &&
+        cells[7].innerHTML == "X" &&
+        cells[8].innerHTML == "X") ||
+      (cells[0].innerHTML == "X" &&
+        cells[4].innerHTML == "X" &&
+        cells[8].innerHTML == "X") ||
+      (cells[2].innerHTML == "X" &&
+        cells[4].innerHTML == "X" &&
+        cells[6].innerHTML == "X")
+    ) {
+      turnP.innerText = "X Wins :)";
+    }
+    if (
+      (cells[0].innerHTML == "O" &&
+        cells[1].innerHTML == "O" &&
+        cells[2].innerHTML == "O") ||
+      (cells[0].innerHTML == "O" &&
+        cells[3].innerHTML == "O" &&
+        cells[6].innerHTML == "O") ||
+      (cells[1].innerHTML == "O" &&
+        cells[4].innerHTML == "O" &&
+        cells[7].innerHTML == "O") ||
+      (cells[2].innerHTML == "O" &&
+        cells[5].innerHTML == "O" &&
+        cells[8].innerHTML == "O") ||
+      (cells[3].innerHTML == "O" &&
+        cells[4].innerHTML == "O" &&
+        cells[5].innerHTML == "O") ||
+      (cells[6].innerHTML == "O" &&
+        cells[7].innerHTML == "O" &&
+        cells[8].innerHTML == "O") ||
+      (cells[0].innerHTML == "O" &&
+        cells[4].innerHTML == "O" &&
+        cells[8].innerHTML == "O") ||
+      (cells[2].innerHTML == "O" &&
+        cells[4].innerHTML == "O" &&
+        cells[6].innerHTML == "O")
+    ) {
+      turnP.innerText = "O Wins :)";
+    }
+  };
+}
